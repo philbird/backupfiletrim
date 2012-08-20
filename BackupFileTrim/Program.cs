97ebef35ee04180d2079b8e621eb3e55b3129a15
philbird <phil.bird@gmail.com>
2012-08-20 07:11:57 +0100

    first commit
    Nothing really in here, but it's more a test for github for windows.

---
+++ BackupFileTrim/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupFileTrim
+{
+    class Program
+    {
+
+        
+
+        static void Main(string[] args)
+        {
+            string StartDirectory = ""; 
+            if (args.Length > 0)
+            {
+                StartDirectory = args[0];
+ 
+
+            }
+        }
+    }
+}
